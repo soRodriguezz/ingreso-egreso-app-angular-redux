edb4f37117b52cb6ad525ecb66fca72e82ecea4f
soRodriguezz <s.rodriguezzapata01@gmail.com>
2021-12-18 22:39:03 -0300

    sweet alert timer

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-register',
@@ -27,13 +28,25 @@ export class RegisterComponent implements OnInit {
 
     if (this.registroForm.invalid) { return; }
 
+    Swal.fire({
+      title: 'Espere por favor',
+      didOpen: () => {
+        Swal.showLoading()
+      }
+    });
+
     const { nombre, correo, password } = this.registroForm.value;
     
     this.authService.crearUsuario(nombre, correo, password).then(credenciales => {
       console.log(credenciales);
+      Swal.close();
       this.router.navigate(['/']);
     }).catch(err => {
-      console.log(err);
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: err.message
+      });
     });
   }
 
