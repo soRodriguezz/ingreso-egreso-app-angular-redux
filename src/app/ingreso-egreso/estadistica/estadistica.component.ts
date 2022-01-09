1e7e5db3f900237075359fdc9112a2007ec65ab9
soRodriguezz <s.rodriguezzapata01@gmail.com>
2022-01-09 01:05:40 -0300

    cambio nombre dashboard

--- src/app/ingreso-egreso/estadistica/estadistica.component.ts
+++ src/app/ingreso-egreso/estadistica/estadistica.component.ts
@@ -34,6 +34,11 @@ export class EstadisticaComponent implements OnInit {
   }
 
   generarEstadistica( items: IngresoEgreso[]){
+    this.totalEgresos = 0;
+    this.totalIngresos = 0;
+    this.ingresos = 0;
+    this.egresos = 0;
+    
     for( const item of items ) {
       if ( item.tipo === 'ingreso' ) {
         this.totalIngresos += item.monto!;
--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
+import { filter } from 'rxjs/operators';
+import { AppState } from 'src/app/app.reducer';
 import { AuthService } from 'src/app/services/auth.service';
 import Swal from 'sweetalert2';
 
@@ -9,27 +13,44 @@ import Swal from 'sweetalert2';
   styles: [
   ]
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent implements OnInit, OnDestroy {
 
-  constructor( private authService: AuthService, private router: Router) { }
+  nombre: string = '';
+  userSubs!: Subscription;
 
+  constructor( 
+    private authService: AuthService, 
+    private router: Router,
+    private store: Store<AppState>  
+  ) { }
+  
   ngOnInit(): void {
+    this.userSubs = this.store.select('user')
+      .pipe(
+        filter( ({ user }) => user != null )
+      )
+      .subscribe( ({user}) => {
+          this.nombre = user!.nombre;
+      }); 
   }
-
+  
   logout() {
-
+    
     Swal.fire({
       title: 'Cerrando sesión',
       didOpen: () => {
         Swal.showLoading()
       }
     });
-
+    
     this.authService.logout().then(() => {
       Swal.close();
       this.router.navigate(['/login']);
     });
   }
-
-
+  
+  ngOnDestroy(): void {
+    this.userSubs.unsubscribe();
+  }
+  
 }
